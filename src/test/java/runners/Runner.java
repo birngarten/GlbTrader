0741d1a4d5b27ce28514609c9407bc9c3b46facd
birngarten <resitozyurt@gmail.com>
2020-10-20 20:29:17 +0200

    pull

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@package074",
+        tags = "",
         dryRun = false
 
 
